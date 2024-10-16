4bc3331d839837202fe61699baab81e7e4fd3016
Eunice <eunicecristina0510@gmail.com>
2024-10-15 21:10:24 -0300

    commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pymysql
+
+app = Flask(__name__)
+
+db_config = {
+    'host' : 'localhost',
+    'user' : 'root',
+    'password' : '',
+    'database' : 'db_projeto',
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/registro', methods=['GET','POST'])
+def registro():
+    if request.method=='POST':
+        conn = pymysql.connect(**db_config)
+        cursor = conn.cursor()
+
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        cursor.execute('INSERT INTO tb_usuarios (usr_nome,usr_email,usr_senha) VALUES (%s,%s,%s)', (nome,email,senha))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('index'))
+    return render_template('registro.html')
+
+
