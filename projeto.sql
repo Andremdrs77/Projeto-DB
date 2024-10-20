8f9a38fd14ff198a32b71fb3f3a2edc400860acc
André Medeiros Silva <andre1998vv@gmail.com>
2024-10-20 20:16:32 -0300

    Update DB

--- projeto.sql
+++ projeto.sql
@@ -4,18 +4,18 @@ use db_projeto;
 
 create table tb_usuarios(
 usr_id int primary key auto_increment not null,
-usr_nome varchar(50),
+usr_nome varchar(255),
 usr_email text,
-usr_senha varchar(50));
+usr_senha varchar(255));
 
 create table tb_tarefas(
 tar_id int primary key auto_increment not null,
-tar_nome varchar(50),
+tar_categoria varchar(255)
+tar_nome varchar(255),
 tar_descricao text,
 tar_data date,
 tar_data_limite date,
 tar_usr_id int not null,
-tar_status text,
-foreign key (tar_usr_id) references tb_usuarios(usr_id));
-
-
+tar_status varchar(255),
+tar_prioridade varchar(255)
+foreign key (tar_usr_id) references tb_usuarios(usr_id)); 
